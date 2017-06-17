f6fc97bb4e367b391360e4270036768ea0e08902
shenhui.yang <shenhui.yang@wz-inc.com>
2017-06-17 15:41:14 +0800

    add common/src/main/java/com/vermouth/common/utils/IPUtils.java getIpAddress

--- common/src/main/java/com/vermouth/common/utils/IPUtils.java
+++ common/src/main/java/com/vermouth/common/utils/IPUtils.java
@@ -1,12 +1,76 @@
 package com.vermouth.common.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Random;
 
 /**
  * Created by shenhui.ysh on 2017/6/16 0016.
  */
 public class IPUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IPUtils.class);
     private IPUtils() {}
+
+    /**
+     * 获取请求主机IP地址,如果通过代理进来，则透过防火墙获取真实IP地址;
+     *
+     * @param request
+     * @return
+     * @throws IOException
+     */
+    public final static String getIpAddress(HttpServletRequest request) throws IOException {
+        // 获取请求主机IP地址，如果通过代理进来，则透过防火墙获取真实IP地址
+        String ip = request.getHeader("X-Forwarded-For");
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("Proxy-Client-IP");
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("WL-Proxy-Client-IP");
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_CLIENT_IP");
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);
+                }
+            }
+            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                ip = request.getRemoteAddr();
+                if (LOGGER.isInfoEnabled()) {
+                    LOGGER.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);
+                }
+            }
+        } else if (ip.length() > 15) {
+            String[] ips = ip.split(",");
+            for (int index = 0; index < ips.length; index++) {
+                String strIp = ips[index];
+                if (!("unknown".equalsIgnoreCase(strIp))) {
+                    ip = strIp;
+                    break;
+                }
+            }
+        }
+        return ip;
+    }
 
     /**
      * 随机生成国内IP地址
