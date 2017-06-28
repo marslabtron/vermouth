1ca2998481b8c41896bea9c40e92db2cfc7e2185
shenhui.yang <shenhui.yang@wz-inc.com>
2017-06-28 17:52:39 +0800

    update

--- common/src/main/java/com/vermouth/common/utils/IPUtils.java
+++ common/src/main/java/com/vermouth/common/utils/IPUtils.java
@@ -21,55 +21,60 @@ public class IPUtils {
      * @return
      * @throws IOException
      */
-    public final static String getIpAddress(HttpServletRequest request) throws IOException {
-        // 获取请求主机IP地址，如果通过代理进来，则透过防火墙获取真实IP地址
-        String ip = request.getHeader("X-Forwarded-For");
-        if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);
-        }
+    public final static String getIpAddress(HttpServletRequest request) {
+        try {
+            // 获取请求主机IP地址，如果通过代理进来，则透过防火墙获取真实IP地址
+            String ip = request.getHeader("X-Forwarded-For");
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info("getIpAddress(HttpServletRequest) - X-Forwarded-For - String ip=" + ip);
+            }
 
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("Proxy-Client-IP");
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("Proxy-Client-IP");
+                    if (LOGGER.isInfoEnabled()) {
+                        LOGGER.info("getIpAddress(HttpServletRequest) - Proxy-Client-IP - String ip=" + ip);
+                    }
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("WL-Proxy-Client-IP");
+                    if (LOGGER.isInfoEnabled()) {
+                        LOGGER.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);
+                    }
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("HTTP_CLIENT_IP");
+                    if (LOGGER.isInfoEnabled()) {
+                        LOGGER.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);
+                    }
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+                    if (LOGGER.isInfoEnabled()) {
+                        LOGGER.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);
+                    }
+                }
+                if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+                    ip = request.getRemoteAddr();
+                    if (LOGGER.isInfoEnabled()) {
+                        LOGGER.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);
+                    }
+                }
+            } else if (ip.length() > 15) {
+                String[] ips = ip.split(",");
+                for (int index = 0; index < ips.length; index++) {
+                    String strIp = ips[index];
+                    if (!("unknown".equalsIgnoreCase(strIp))) {
+                        ip = strIp;
+                        break;
+                    }
                 }
             }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("WL-Proxy-Client-IP");
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("getIpAddress(HttpServletRequest) - WL-Proxy-Client-IP - String ip=" + ip);
-                }
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_CLIENT_IP");
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("getIpAddress(HttpServletRequest) - HTTP_CLIENT_IP - String ip=" + ip);
-                }
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("getIpAddress(HttpServletRequest) - HTTP_X_FORWARDED_FOR - String ip=" + ip);
-                }
-            }
-            if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-                ip = request.getRemoteAddr();
-                if (LOGGER.isInfoEnabled()) {
-                    LOGGER.info("getIpAddress(HttpServletRequest) - getRemoteAddr - String ip=" + ip);
-                }
-            }
-        } else if (ip.length() > 15) {
-            String[] ips = ip.split(",");
-            for (int index = 0; index < ips.length; index++) {
-                String strIp = ips[index];
-                if (!("unknown".equalsIgnoreCase(strIp))) {
-                    ip = strIp;
-                    break;
-                }
-            }
+            return ip;
+        }catch (Throwable t) {
+            t.printStackTrace();
         }
-        return ip;
+        return null;
     }
 
     /**
